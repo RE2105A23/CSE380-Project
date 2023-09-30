3d13a027260e39bc58d16b01e3f4ca8f74185201
SRI VISHNU JSB <54164748+SRIVISHNU-JSB@users.noreply.github.com>
2023-09-30 17:24:42 +0530

    Initial Test Setup 1

---
+++ smartSMS/test/java/modelsTest/UserTest.java
@@ -0,0 +1,18 @@
+package modelsTest;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class UserTest {
+
+    @Test
+    public void testAuthenticate() {
+        // Your test code here
+    }
+
+    @Test
+    public void testGetName() {
+        // Your test code here
+    }
+
+    // Add more test methods as needed
+}
